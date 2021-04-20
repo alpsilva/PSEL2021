3b4d2ba41d906b1f85899c048fc576927f8656be
André Luiz <alps2@cin.ufpe.br>
2021-04-20 15:21:52 -0300

    walk forward, backward and rotate left or right. Beylade implemented

--- ProjetoPSEL.pro
+++ ProjetoPSEL.pro
@@ -36,12 +36,8 @@ SOURCES += \
         modules/module.cpp \
         modules/vision/vision.cpp \
         protobufs/command.pb.cc \
-        protobufs/command.pb.cc \
-        protobufs/common.pb.cc \
         protobufs/common.pb.cc \
         protobufs/packet.pb.cc \
-        protobufs/packet.pb.cc \
-        protobufs/replacement.pb.cc \
         protobufs/replacement.pb.cc
 
 # Default rules for deployment.
@@ -54,11 +50,7 @@ HEADERS += \
     modules/module.h \
     modules/vision/vision.h \
     protobufs/command.pb.h \
-    protobufs/command.pb.h \
-    protobufs/common.pb.h \
     protobufs/common.pb.h \
     protobufs/packet.pb.h \
-    protobufs/packet.pb.h \
-    protobufs/replacement.pb.h \
     protobufs/replacement.pb.h
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <thread>
 #include <modules/actuator/actuator.h>
 #include <modules/vision/vision.h>
-
 #include <unistd.h>
 
 using namespace std;
@@ -17,15 +16,15 @@ int main(int argc, char *argv[]) {
     // Desired frequency
     int desiredFrequency = 60;
 
-    float v = 3; //Velocidade de movimento manual
-    float vw = 2; //Velocidade de rotação manual
+    float v = 100; //Velocidade de movimento manual
+    float vw = 100; //Velocidade de rotação manual
 
     bool isYellow;
     int chosenID;
 
     auto manualMove = [](Actuator *actuator, bool isYellow, int chosenID, float v){
         actuator->sendCommand(isYellow, chosenID, v, v);
-        usleep(100000);//sleeps for 1 second
+        usleep(200000);//sleeps for 2 second
         actuator->sendCommand(isYellow, chosenID, 0, 0);
     };
 
@@ -36,7 +35,7 @@ int main(int argc, char *argv[]) {
             actuator->sendCommand(isYellow, chosenID, v, 0);
         }
 
-        usleep(100000);//sleeps for 1 second
+        usleep(200000);//sleeps for 2 second
         actuator->sendCommand(isYellow, chosenID, 0, 0);
     };
 
@@ -44,77 +43,68 @@ int main(int argc, char *argv[]) {
         // TimePoint
         std::chrono::high_resolution_clock::time_point beforeProcess = std::chrono::high_resolution_clock::now();
 
-        std::cout << "Qual modo deseja usar? 'a' para automático, 'm' para manual:" << std::endl;
-        char modo = '0';
-        cin >> modo;
-        if (modo == 'a'){
-            std::cout << "O amarelo 3 ficará chutando ao gol!" << std::endl;
+        // Process vision and actuator commands
+        vision->processNetworkDatagrams();
 
-            while(true) {
-
-                    // TimePoint
-                    std::chrono::high_resolution_clock::time_point afterProcess = std::chrono::high_resolution_clock::now();
-
-                    // Sleep thread
-                    long remainingTime = (1000 / desiredFrequency) - (std::chrono::duration_cast<std::chrono::milliseconds>(afterProcess - beforeProcess)).count();
-                    std::this_thread::sleep_for(std::chrono::milliseconds(remainingTime));
-                }
-
-        }
-
-        else if (modo == 'm'){
-            while (true){
-                //selecionar time
-                char team = '0';
-                while (team != 'y' && team != 'b'){
-                    std::cout << "Qual time deseja usar? 'y' para yellow, 'b' para azul:" << std::endl;
-                    cin >> team;
-                    if (team == 'y'){
-                        isYellow = true;
-                    } else if (team == 'b'){
-                        isYellow = false;
-                    }
-                }
-
-                //Selecionar o robô
-                chosenID = -1;
-                while(!(chosenID >= 0 && chosenID <= 3)){
-                    std::cout << "Qual robô deseja controlar? Digite o seu ID (0 a 3):" << std::endl;
-                    cin >> chosenID;
-                }
-
-                //comandos:
-                //Para que o comando seja tratado como um impulso, e não como uma velocidade constante,
-                //fiz o programa pausar por um tempo, depois passei um novo comando zerando as velocidades após o impulso.
-                std::cout << "Você já pode controlar o robô! ws para movimentação, qe para rotação." << std::endl;
-                std::cout << "l para mostrar posição do robô (Pode demorar um pouco para a vision atualizar)." << std::endl;
-                std::cout << "p para parar." << std::endl;
-
-                char command = 's';
-
-                while (command != 'p'){
-
-                    //Roda a visão
-                    vision->processNetworkDatagrams();
-
-                    fira_message::Robot robot = vision->getLastRobotDetection(isYellow, chosenID);
-
-                    cin >> command;
-
-                    if (command == 'w'){
-                        manualMove(actuator, isYellow, chosenID, v);
-                    } else if (command == 's') {
-                        manualMove(actuator, isYellow, chosenID, -v);
-                    } else if (command == 'q'){
-                        manualRotation(actuator, isYellow, chosenID, vw, true);
-                    } else if (command == 'e'){
-                        manualRotation(actuator, isYellow, chosenID, vw, false);
-                    } else if (command == 'l'){
-                        std::cout << robot.x() << " | " << robot.y() << std::endl;
-                    }
+        while (true){
+            //selecionar time
+            char team = '0';
+            while (team != 'y' && team != 'b'){
+                std::cout << "Qual time deseja usar? 'y' para yellow, 'b' para azul:" << std::endl;
+                cin >> team;
+                if (team == 'y'){
+                    isYellow = true;
+                } else if (team == 'b'){
+                    isYellow = false;
                 }
             }
 
+            //Selecionar o robô
+            chosenID = -1;
+            while(!(chosenID >= 0 && chosenID <= 2)){
+                std::cout << "Qual robô deseja controlar? Digite o seu ID (0 a 2):" << std::endl;
+                cin >> chosenID;
+            }
+
+            //comandos:
+            //Para que o comando seja tratado como um impulso, e não como uma velocidade constante,
+            //fiz o programa pausar por um tempo, depois passei um novo comando zerando as velocidades após o impulso.
+            std::cout << "Você já pode controlar o robô! ws para movimentação, qe para rotação." << std::endl;
+            std::cout << "l para mostrar posição do robô (Pode demorar um pouco para a vision atualizar)." << std::endl;
+            std::cout << "x to let it rip." << std::endl;
+            std::cout << "p para parar." << std::endl;
+
+            char command = 's';
+            bool beyblade = false;
+            while (command != 'p'){
+
+                //Roda a visão
+                vision->processNetworkDatagrams();
+
+                fira_message::Robot robot = vision->getLastRobotDetection(isYellow, chosenID);
+
+                cin >> command;
+
+                if (command == 'w'){
+                    manualMove(actuator, isYellow, chosenID, v);
+                } else if (command == 's') {
+                    manualMove(actuator, isYellow, chosenID, -v);
+                } else if (command == 'q'){
+                    manualRotation(actuator, isYellow, chosenID, vw, true);
+                } else if (command == 'e'){
+                    manualRotation(actuator, isYellow, chosenID, vw, false);
+                } else if (command == 'x'){
+                    if (beyblade){
+                        actuator->sendCommand(isYellow, chosenID, 0, 0);
+                        beyblade = false;
+                    } else {
+                        actuator->sendCommand(isYellow, chosenID, 3 * v, -3 * v);
+                        beyblade = true;
+                    }
+                } else if (command == 'l'){
+                    std::cout << robot.x() << " | " << robot.y() << std::endl;
+                }
+            }
         }
 
 
