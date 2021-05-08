aaf4c416691ac64f68311f136d607f5f35e25155
André Luiz <alps2@cin.ufpe.br>
2021-05-08 15:41:27 -0300

    refactor

--- main.cpp
+++ main.cpp
@@ -9,11 +9,9 @@
 
 using namespace std;
 
-float getPlayerRotateAngleTo(float px, float py, fira_message::Robot robot) {
-    float rx = robot.x();
-    float ry = robot.y();
-    float componentX = (px - rx);
-    float componentY = (py - ry);
+float getPlayerRotateAngleTo(float rx, float ry, float ori, float tx, float ty) {
+    float componentX = (tx - rx);
+    float componentY = (ty - ry);
     float distToTarget = sqrt(pow(componentX, 2) + pow(componentY, 2));
 
     componentX = componentX / distToTarget;
@@ -32,7 +30,7 @@ float getPlayerRotateAngleTo(float px, float py, fira_message::Robot robot) {
         angleOriginToTarget = acos(componentX); // Angle that the target make with x-axis to robot
     }
 
-    angleRobotToTarget = angleOriginToTarget - robot.orientation();
+    angleRobotToTarget = angleOriginToTarget - ori;
 
     // Adjusting to rotate the minimum possible
     if(angleRobotToTarget > M_PI) angleRobotToTarget -= 2.0 * M_PI;
@@ -40,6 +38,50 @@ float getPlayerRotateAngleTo(float px, float py, fira_message::Robot robot) {
 
     return angleRobotToTarget;
 }
+
+float getVx (float rx, float ry, float ori, float tx, float ty) {
+    float dx = (tx - rx);
+    float dy = (ty - ry);
+
+    //Função usada no omni
+    float vx = (dx * cos(ori) + dy * sin(ori));
+    //float vy = (dy * cos(ori) - dx * sin(ori));
+
+    //vx = vx/500;
+    //vy = vy/500;
+
+    if (vx > 0){
+        vx = sqrt(vx);
+    }
+    /*
+    if (vy > 0){
+        vy = sqrt(vy);
+    }
+    */
+
+    return vx;
+}
+
+QPair<float, float> getWheelVelocities (float vx, float angle, float r, float l) {
+
+    float vl = (2*vx) - (l*angle)/(2*r);
+    float vr = (2*vx) + (l*angle)/(2*r);
+
+    QPair<float, float> wheelVelocities;
+    wheelVelocities.first = vl;
+    wheelVelocities.second = vr;
+
+    return wheelVelocities;
+}
+
+
+/*
+float calculateProportion(float angle, float vx){
+    float v = 3/pow(angle+1, 2);
+    return v * vx;
+}
+*/
+
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
@@ -50,90 +92,22 @@ int main(int argc, char *argv[]) {
     // Desired frequency
     int desiredFrequency = 60;
 
-    float v = 100; //Velocidade de movimento manual
-    float vw = 5; //Velocidade de rotação manual
+    float raio = 0; //Raio das rodas
+    float distEntreRodas = 0; //Distância entre as rodas
 
-    bool isYellow;
-    int chosenID;
+    float vl; //Velocidade da roda esquerda
+    float vr; //Velocidade da roda direita
 
-    auto facePoint = [](Vision *vision, Actuator *actuator, bool isYellow, int chosenID, bool pointIsBall, float px, float py){
-        vision->processNetworkDatagrams();
-        //pegando instância atual do robô selecionado
-        fira_message::Robot robot = vision->getLastRobotDetection(isYellow, chosenID);
-        //pegando instância atual da posição da bola
-        fira_message::Ball ball = vision->getLastBallDetection();
+    bool isYellow = true;
+    int chosenID = 0;
 
-        //Se true, ele vira para bola. Falso, ele vira para o gol.
-        if (pointIsBall){
-            px = ball.x();
-            py = ball.y();
-        }
+    float angleError = 0.05; //aprox. 3º
 
-        //Angulo necessário para o robô apontar
-        float angle = getPlayerRotateAngleTo(px, py, robot);
+    //Teste para fazer o robô ir até o centro
+    float moveTargetx = 0;
+    float moveTargety = 0;
 
-        //O robô parace sempre "fugir" da ponto, então temos que adcionar 180º ao angulo desejado
-        //No caso de radianos, 180º == pi
-
-        angle = angle + M_PI;
-
-        if (angle > (M_PI)){
-            float resto = angle - M_PI;
-            angle = -M_PI + resto;
-        }
-
-        //Orientação atual do robô
-        float orientation = robot.orientation();
-
-        float vw2; //velocidade angular dentro dessa função
-
-        if (pointIsBall){
-            vw2 = 2.5;
-        } else{
-            vw2 = 1;
-        }
-
-        //Faz o robô girar
-        if (orientation > angle){
-            actuator->sendCommand(isYellow, chosenID, vw2, -vw2);
-        } else{
-            actuator->sendCommand(isYellow, chosenID, -vw2, vw2);
-        }
-
-        float variacao = 0.002; //faixa de angulação aceitável para parar o loop
-        float faixaInf = angle-variacao;
-        float faixaSup = angle+variacao;
-
-
-        //Robô roda enquanto a orientação dele não estiver perto o bastante da variavel angle
-        while((orientation < faixaInf) || (orientation > faixaSup)){
-            vision->processNetworkDatagrams();
-            robot = vision->getLastRobotDetection(isYellow, chosenID);
-            orientation = robot.orientation();
-            std::cout << "Angulo necessário: " << angle << std::endl;
-            std::cout << "Orientação do robô: " << orientation << std::endl;
-        }
-
-        //para de rodar
-        actuator->sendCommand(isYellow, chosenID, 0, 0);
-    };
-
-    auto manualMove = [](Actuator *actuator, bool isYellow, int chosenID, float v){
-        actuator->sendCommand(isYellow, chosenID, v, v);
-        usleep(200000);//sleeps for 2 second
-        actuator->sendCommand(isYellow, chosenID, 0, 0);
-    };
-
-    auto manualRotation = [](Actuator *actuator, bool isYellow, int chosenID, float v, bool isLeft){
-        if (isLeft){
-            actuator->sendCommand(isYellow, chosenID, -v, v);
-        } else {
-            actuator->sendCommand(isYellow, chosenID, v, -v);
-        }
-
-        usleep(200000);//sleeps for 2 second
-        actuator->sendCommand(isYellow, chosenID, 0, 0);
-    };
+    bool isInCenter = false;
 
     while(true) {
         // TimePoint
@@ -142,72 +116,34 @@ int main(int argc, char *argv[]) {
         // Process vision and actuator commands
         vision->processNetworkDatagrams();
 
-        while (true){
-            //selecionar time
-            char team = '0';
-            while (team != 'y' && team != 'b'){
-                std::cout << "Qual time deseja usar? 'y' para yellow, 'b' para azul:" << std::endl;
-                cin >> team;
-                if (team == 'y'){
-                    isYellow = true;
-                } else if (team == 'b'){
-                    isYellow = false;
-                }
-            }
+        fira_message::Robot robot = vision->getLastRobotDetection(isYellow, chosenID);
 
-            //Selecionar o robô
-            chosenID = -1;
-            while(!(chosenID >= 0 && chosenID <= 2)){
-                std::cout << "Qual robô deseja controlar? Digite o seu ID (0 a 2):" << std::endl;
-                cin >> chosenID;
-            }
+        float rx = robot.x();
+        float ry = robot.y();
+        float orientation = robot.orientation();
 
-            //comandos:
-            //Para que o comando seja tratado como um impulso, e não como uma velocidade constante,
-            //fiz o programa pausar por um tempo, depois passei um novo comando zerando as velocidades após o impulso.
-            std::cout << "Você já pode controlar o robô! ws para movimentação, qe para rotação." << std::endl;
-            std::cout << "l para mostrar posição do robô (Pode demorar um pouco para a vision atualizar)." << std::endl;
-            std::cout << "b para olhar para a bola." << std::endl;
-            std::cout << "x to let it rip." << std::endl;
-            std::cout << "p para parar." << std::endl;
+        fira_message::Ball ball = vision->getLastBallDetection();
 
-            char command = 's';
-            bool beyblade = false;
-            while (command != 'p'){
+        float bx = ball.x();
+        float by = ball.y();
 
-                //Roda a visão
-                vision->processNetworkDatagrams();
 
-                fira_message::Robot robot = vision->getLastRobotDetection(isYellow, chosenID);
+        //Angulação: é preciso que o robô rotacione até esse ponto.
+        float angleRobotToObjective = getPlayerRotateAngleTo(rx, ry, orientation, moveTargetx, moveTargety);
 
-                cin >> command;
+        if(orientation > M_PI) orientation -= 2.0 * M_PI;
+        if(orientation < -M_PI) orientation += 2.0 * M_PI;
 
-                if (command == 'w'){
-                    manualMove(actuator, isYellow, chosenID, -v);
-                } else if (command == 's') {
-                    manualMove(actuator, isYellow, chosenID, v);
-                } else if (command == 'q'){
-                    manualRotation(actuator, isYellow, chosenID, vw, false);
-                } else if (command == 'e'){
-                    manualRotation(actuator, isYellow, chosenID, vw, true);
-                } else if (command == 'b'){
-                    facePoint(vision, actuator, isYellow, chosenID, true, 0, 0);
-                } else if (command == 'x'){
-                    if (beyblade){
-                        actuator->sendCommand(isYellow, chosenID, 0, 0);
-                        beyblade = false;
-                    } else {
-                        actuator->sendCommand(isYellow, chosenID, 3 * v, -3 * v);
-                        beyblade = true;
-                    }
-                } else if (command == 'l'){
-                    std::cout << robot.x() << " | " << robot.y() << std::endl;
-                    float nearest = roundf(robot.orientation() * 100) / 100;
-                    std::cout << nearest << std::endl;
-                }
-            }
-        }
+        float angleRobotToTarget = orientation + angleRobotToObjective;
 
+        float v = getVx(rx, ry, orientation, moveTargetx, moveTargety);
+
+        QPair<float, float> wheelVelocities = getWheelVelocities(v, angleRobotToTarget, raio, distEntreRodas);
+
+        vl = wheelVelocities.first;
+        vr = wheelVelocities.second;
+
+        actuator->sendCommand(isYellow, chosenID, vl, vr);
 
         // TimePoint
         std::chrono::high_resolution_clock::time_point afterProcess = std::chrono::high_resolution_clock::now();
